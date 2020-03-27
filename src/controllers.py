9a7933fb41d007cfd1cc280df1101b608da8ec54
oh-yeah-sea-kit2 <spyroach.204@gmail.com>
2020-03-27 14:38:44 +0900

    Basic認証追加

--- src/controllers.py
+++ src/controllers.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
 
 import db
 from models import User, Task
+
+import hashlib
 
 app = FastAPI(
     title='FastAPIでつくるtoDoアプリケーション',
@@ -11,6 +16,7 @@ app = FastAPI(
     version='0.9 beta'
 )
 
+security = HTTPBasic()
 
 # new テンプレート関連の設定 (jinja2)
 templates = Jinja2Templates(directory="templates")
@@ -20,12 +26,28 @@ jinja_env = templates.env  # Jinja2.Environment : filterやglobalの設定用
 def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
-def admin(request: Request):
-    # ユーザとタスクを取得
-    # とりあえずadminユーザのみ取得
+def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    # Basic認証
+    username = credentials.username
+    password = hashlib.md5(credentials.password.encode()).hexdigest()
+
+    # dbよりユーザ名と一致するデータを取得
     user = db.session.query(User).filter(User.username == 'admin').first()
-    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    if user is not None:
+        task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    else:
+        task = []
+    
     db.session.close()
+
+    # 該当ユーザなし
+    if user is None or user.password != password:
+        error = 'ユーザ名か、パスワードが異なっています'
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail=error,
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
     res = templates.TemplateResponse(
         'admin.html', {
--- src/urls.py
+++ src/urls.py
@@ -4,4 +4,3 @@ from controllers import *
 # FastAPIのルーティング用関数
 app.add_api_route('/', index)
 app.add_api_route('/admin', admin)
-
